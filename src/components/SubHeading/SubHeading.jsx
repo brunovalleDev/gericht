635bddbd163969c2db6a452ad7fe3729af3be6ab
Bruno Valle Silva <brunovallesilva93@gmail.com>
2024-01-31 16:16:19 -0300

    Seção About Us finalizada

--- src/components/SubHeading/SubHeading.jsx
+++ src/components/SubHeading/SubHeading.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
+import './subHeading.css'
+import images from '../../constants/images';
 
-const SubHeading = () => (
-  <div>
-    SubHeading
+const SubHeading = ({ title }) => (
+  <div className="app__header-content_subheading">
+    <h2>{title}</h2>
+    <img src={images.spoon} alt="subheading__spoon" />
   </div>
 );
 
--- src/container/AboutUs/AboutUs.jsx
+++ src/container/AboutUs/AboutUs.jsx
@@ -1,10 +1,35 @@
 import React from 'react';
+import { SubHeading } from '../../components/index';
 
 import './AboutUs.css';
+import images from '../../constants/images';
 
 const AboutUs = () => (
-  <div>
-    AboutUs
+  <div className="app__aboutus">
+    <div className="app__aboutus-bg">
+      <img src={images.G} alt="aboutus__g" />
+    </div>
+    <div className="app__aboutus-about">
+      <div className="app__aboutus-about_heading">
+        <h2>About Us</h2>
+        <img src={images.spoon} alt="about__spoon" />
+      </div>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quis pharetra adipiscing ultrices vulputate posuere tristique. In sed odio nec aliquet eu proin mauris et.</p>
+      <button type="button">Know More</button>
+    </div>
+
+    <div className="app__aboutus-img">
+      <img src={images.knife} alt="aboutus__knife" />
+    </div>
+
+    <div className="app__aboutus-history">
+      <div className="app__aboutus-history_heading">
+        <h2>Our History</h2>
+        <img src={images.spoon} alt="about__spoon" />
+      </div>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quis pharetra adipiscing ultrices vulputate posuere tristique. In sed odio nec aliquet eu proin mauris et.</p>
+      <button type="button">Know More</button>
+    </div>
   </div>
 );
 
