895f3aa018a11fcd5520acd700a4272acc6272d5
Bruno Valle Silva <brunovallesilva93@gmail.com>
2024-02-02 15:37:02 -0300

    Seção Laurels finalizada

--- src/container/Intro/Intro.jsx
+++ src/container/Intro/Intro.jsx
@@ -1,11 +1,42 @@
-import React from 'react';
+import React from 'react'
+import { useState, useRef, useEffect } from 'react';
+import { meal } from '../../constants';
+import { BsFillPlayFill, BsPauseFill } from 'react-icons/bs';
+import './Intro.css'
 
-import './Intro.css';
+const Intro = () => {
+  const [playVideo, setPlayVideo] = useState(false);
+  const videoRef = useRef();
 
-const Intro = () => (
-  <div>
-    Intro
-  </div>
-);
-
-export default Intro;
+  useEffect(() => {
+    if (playVideo) {
+      videoRef.current.play();
+    } else {
+      videoRef.current.pause();
+    }
+  })
+  return (
+    <div className="app__video">
+      <video
+        ref={videoRef}
+        src={meal}
+        muted
+        loop
+        controls={false}
+        type="video/mp4"
+      />
+      <div className="app__video-overlay">
+        <div className="app__video-overlay_circle" onClick={() => { setPlayVideo(!playVideo) }}>
+          {
+            playVideo
+              ?
+              (<BsPauseFill color="#fff" size={30} />)
+              :
+              (<BsFillPlayFill color="#fff" size={30} />)
+          }
+        </div>
+      </div>
+    </div>
+  )
+}
+export default Intro
--- src/container/Laurels/Laurels.jsx
+++ src/container/Laurels/Laurels.jsx
@@ -1,11 +1,47 @@
 import React from 'react';
-
+import { SubHeading } from '../../components/index.js'
+import data from '../../constants/data.js';
 import './Laurels.css';
+import images from '../../constants/images.js';
 
-const Laurels = () => (
-  <div>
-    Laurels
-  </div>
-);
+const AwardCard = ({ award: { imgUrl, title, subtitle } }) => {
+  return (
+    <div className="app__laurels-content_awards-card">
+      <div className="app__laurels-content_awards-card_img">
+        <img src={imgUrl} alt='laurels__card' />
+      </div>
+
+      <div className="app__laurels-content_awards-card_description">
+        <h4>{title}</h4>
+        <p>{subtitle}</p>
+      </div>
+    </div>
+  )
+}
+
+const Laurels = () => {
+  return (
+    <div class="app__laurels">
+      <div class="app__laurels-content">
+        <div class="app__laurels-content_heading">
+          <SubHeading title="Awards & recognition" />
+          <h2>Our Laurels</h2>
+        </div>
+
+        <div className="app__laurels-content_awards">
+          {
+            data.awards.map((award, index) =>
+              <AwardCard award={award} key={`${award.title} - ${index}`} />
+            )
+          }
+        </div>
+      </div>
+
+      <div class="app__laurels-img">
+        <img src={images.laurels} alt="laurels__img" />
+      </div>
+    </div>
+  )
+};
 
 export default Laurels;
