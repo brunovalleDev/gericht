f31698d4594d6312e48ae6a86fa51cdab4bb3e83
Bruno Valle Silva <brunovallesilva93@gmail.com>
2024-02-02 16:24:44 -0300

    Seção FindUs finalizada

--- src/container/Findus/FindUs.jsx
+++ src/container/Findus/FindUs.jsx
@@ -1,8 +1,27 @@
+/* Essa foi a única classe na qual não utilizei um arquivo CSS e sim somente CSS inline e classes que foram pré definidas em App.css. A ideia é mostrar ao recrutrador que sei como utilizar essas classes pré-definidas de forma eficiente. O restante do projeto esta feito sem essas classes. */
+
 import React from 'react';
 
+import { SubHeading } from '../../components';
+import { images } from '../../constants';
+
 const FindUs = () => (
-  <div>
-    FindUs
+  <div className="app__bg app__wrapper section__padding" id="contact">
+    <div className="app__wrapper_info">
+      <SubHeading title="Contact" />
+      <h1 className="headtext__cormorant" style={{ marginBottom: '3rem' }}>Find Us</h1>
+      <div className="app__wrapper-content">
+        <p className="p__opensans">Lane Ends Bungalow, Whatcroft Hall Lane, Rudheath, CW9 75G</p>
+        <p className="p__cormorant" style={{ color: '#DCCA87', margin: '2rem 0' }}>Opening Hours</p>
+        <p className="p__opensans">Mon - Fri: 10:00 am - 02:00 am</p>
+        <p className="p__opensans">Sat - Sun: 10:00 am - 03:00 am</p>
+      </div>
+      <button type="button" className="custom__button" style={{ marginTop: '2rem' }}>Visit Us</button>
+    </div>
+
+    <div className="app__wrapper_img">
+      <img src={images.findus} alt="finus_img" />
+    </div>
   </div>
 );
 
